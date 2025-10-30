eae696af7c8a8531c47811946ceffd857224a893
VDowbensky <harbinger@ukr.net>
2025-10-30 05:53:56 +0200

    0

--- FW/radiodriver/lr2021_reversed/lr20xx_abstract_search_symbols.c
+++ FW/radiodriver/lr2021_reversed/lr20xx_abstract_search_symbols.c
@@ -1,18 +1,12 @@
 
 
-lr20xx_status_t
-abstract_search_symbols(void *context,uint8_t n_symbols,search_symbol_format_t format)
+lr20xx_status_t abstract_search_symbols(void *context,uint8_t n_symbols,search_symbol_format_t format)
 {
-  lr20xx_hal_status_t lVar1;
-  search_symbol_format_t format_local;
-  uint8_t n_symbols_local;
-  void *context_local;
   uint8_t cbuffer [4];
   
-  cbuffer[0] = '\x02';
+  cbuffer[0] = 0x02;
   cbuffer[1] = '\"';
   cbuffer[2] = n_symbols;
   cbuffer[3] = format;
-  lVar1 = lr20xx_hal_write(context,cbuffer,4,(uint8_t *)0x0,0);
-  return lVar1;
+  return lr20xx_hal_write(context,cbuffer,4,NULL,0);
 }
--- FW/radiodriver/lr2021_reversed/lr20xx_hal.c
+++ FW/radiodriver/lr2021_reversed/lr20xx_hal.c
@@ -1,14 +1,9 @@
 
 lr20xx_hal_status_t lr20xx_hal_reset(void *context)
 {
-  void *context_local;
-  lr20xx_hal_context_t *lr20xx_context;
-  
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)context + 0x1c),SMTC_HAL_MCU_GPIO_STATE_LOW);
-  smtc_hal_mcu_wait_ms(1);
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)context + 0x1c),SMTC_HAL_MCU_GPIO_STATE_HIGH);
+  //set RST low
+  //delay 1 ms
+  //set RST high
   return LR20XX_HAL_STATUS_OK;
 }
 
@@ -16,99 +11,58 @@ lr20xx_hal_status_t lr20xx_hal_reset(void *context)
 
 lr20xx_hal_status_t lr20xx_hal_wakeup(void *context)
 {
-  void *context_local;
-  lr20xx_hal_context_t *lr20xx_context;
-  
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)context + 0x10),SMTC_HAL_MCU_GPIO_STATE_LOW);
-  smtc_hal_mcu_wait_ms(1);
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)context + 0x10),SMTC_HAL_MCU_GPIO_STATE_HIGH);
+  //NSS low
+  //delay 1 ms
+  //NSS gigh
   return LR20XX_HAL_STATUS_OK;
 }
 
 
 
-lr20xx_hal_status_t
-lr20xx_hal_write(void *context,uint8_t *command,uint16_t command_length,uint8_t *data,
-                uint16_t data_length)
+lr20xx_hal_status_t lr20xx_hal_write(void *context,uint8_t *command,uint16_t command_length,uint8_t *data,uint16_t data_length)
 {
-  uint8_t *data_local;
-  uint16_t command_length_local;
-  uint8_t *command_local;
-  void *context_local;
-  lr20xx_hal_context_t *lr20xx_context;
-  
   lr20xx_hal_wait_on_busy(context);
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)context + 0x10),SMTC_HAL_MCU_GPIO_STATE_LOW);
-  smtc_hal_mcu_spi_rw_buffer
-            (*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),command,(uint8_t *)0x0,command_length);
-  smtc_hal_mcu_spi_rw_buffer
-            (*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),data,(uint8_t *)0x0,data_length);
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)context + 0x10),SMTC_HAL_MCU_GPIO_STATE_HIGH);
+  //NSS low
+  //send 2 command bytes by SPI
+  //smtc_hal_mcu_spi_rw_buffer(*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),command,NULL,command_length);
+  //send data_length parameters bytes by SPI
+  //smtc_hal_mcu_spi_rw_buffer(*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),data,NULL,data_length);
+  //NSS high
   return LR20XX_HAL_STATUS_OK;
 }
 
 
 
-lr20xx_hal_status_t
-lr20xx_hal_read(void *context,uint8_t *command,uint16_t command_length,uint8_t *data,
-               uint16_t data_length)
+lr20xx_hal_status_t lr20xx_hal_read(void *context,uint8_t *command,uint16_t command_length,uint8_t *data,uint16_t data_length)
 {
-  uint8_t *data_local;
-  uint16_t command_length_local;
-  uint8_t *command_local;
-  void *context_local;
   uint8_t dummy_byte_rx [2];
   uint8_t dummy_byte [2];
-  lr20xx_hal_context_t *lr20xx_context;
   
-  dummy_byte[0] = '\0';
-  dummy_byte[1] = '\0';
-  dummy_byte_rx[0] = '\0';
-  dummy_byte_rx[1] = '\0';
+  dummy_byte[0] = 0;
+  dummy_byte[1] = 0;
+  dummy_byte_rx[0] = 0;
+  dummy_byte_rx[1] = 0;
   lr20xx_hal_wait_on_busy(context);
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)context + 0x10),SMTC_HAL_MCU_GPIO_STATE_LOW);
-  smtc_hal_mcu_spi_rw_buffer
-            (*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),command,(uint8_t *)0x0,command_length);
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)context + 0x10),SMTC_HAL_MCU_GPIO_STATE_HIGH);
+  //NSS low
+  smtc_hal_mcu_spi_rw_buffer(*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),command,NULL,command_length);
+  //NSS high
   lr20xx_hal_wait_on_busy(context);
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)context + 0x10),SMTC_HAL_MCU_GPIO_STATE_LOW);
-  smtc_hal_mcu_spi_rw_buffer
-            (*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),dummy_byte,dummy_byte_rx,2);
-  smtc_hal_mcu_spi_rw_buffer
-            (*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),(uint8_t *)0x0,data,data_length);
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)context + 0x10),SMTC_HAL_MCU_GPIO_STATE_HIGH);
+  //NSS low
+  smtc_hal_mcu_spi_rw_buffer(*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),dummy_byte,dummy_byte_rx,2);
+  smtc_hal_mcu_spi_rw_buffer(*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),NULL,data,data_length);
+  //NSS high
   return LR20XX_HAL_STATUS_OK;
 }
 
 
 
-lr20xx_hal_status_t
-lr20xx_hal_direct_read_fifo
-          (void *radio,uint8_t *command,uint16_t command_length,uint8_t *data,uint16_t data_length)
+lr20xx_hal_status_t lr20xx_hal_direct_read_fifo(void *context,uint8_t *command,uint16_t command_length,uint8_t *data,uint16_t data_length)
 {
-  uint8_t *data_local;
-  uint16_t command_length_local;
-  uint8_t *command_local;
-  void *radio_local;
-  lr20xx_hal_context_t *lr20xx_context;
-  
-  lr20xx_hal_wait_on_busy(radio);
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)radio + 0x10),SMTC_HAL_MCU_GPIO_STATE_LOW);
-  smtc_hal_mcu_spi_rw_buffer
-            (*(smtc_hal_mcu_spi_inst_t *)((int)radio + 4),command,(uint8_t *)0x0,command_length);
-  smtc_hal_mcu_spi_rw_buffer
-            (*(smtc_hal_mcu_spi_inst_t *)((int)radio + 4),(uint8_t *)0x0,data,data_length);
-  smtc_hal_mcu_gpio_set_state
-            (*(smtc_hal_mcu_gpio_inst_t *)((int)radio + 0x10),SMTC_HAL_MCU_GPIO_STATE_HIGH);
+  lr20xx_hal_wait_on_busy(context);
+  //NSS low
+  smtc_hal_mcu_spi_rw_buffer(*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),command,NULL,command_length);
+  smtc_hal_mcu_spi_rw_buffer(*(smtc_hal_mcu_spi_inst_t *)((int)context + 4),NULL,data,data_length);
+  //NSS high
   return LR20XX_HAL_STATUS_OK;
 }
 
